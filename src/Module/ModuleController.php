d445bf9e0f778a1ab665a9e5a7d48fefe5ec2b1c
William Eldridge <wige@comcast.net>
2019-12-20 22:39:46 -0500

    Minor Twig tweaks

--- src/Module/ModuleController.php
+++ src/Module/ModuleController.php
@@ -10,7 +10,15 @@ namespace WigeDev\JasperCore\Module;
  */
 abstract class ModuleController
 {
-    public static function canView()
+    /**
+     * Checks the user permissions to determine if the current user can view this module. If the user can not view the
+     * module, by default the user will be redirected to IndexModule > IndexController > indexAction.
+     * 
+     * Generally, having IndexModule > IndexController return false would be a "bad thing" since that is usually where
+     * the login form is served from.
+     * @return bool
+     */
+    public static function canView(): bool 
     {
         return true;
     }
