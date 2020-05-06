1514356a8d2192766d2f707a6185cadf49c976c5
William Eldridge <wige@comcast.net>
2020-05-06 00:09:54 -0400

    Update to the twigrenderer to allow easier overriding

--- src/Renderer/TwigRenderer.php
+++ src/Renderer/TwigRenderer.php
@@ -8,6 +8,7 @@ use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use Twig\Extension\StringLoaderExtension;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFilter;
 
 /**
  * Class TwigRenderer
@@ -18,6 +19,11 @@ use Twig\Loader\FilesystemLoader;
  */
 class TwigRenderer extends Renderer
 {
+    /** @var Environment */
+    protected $twig;
+    /** @var FilesystemLoader */
+    protected $twig_loader;
+
     /**
      * @param Response $response
      *
@@ -36,14 +42,35 @@ class TwigRenderer extends Renderer
         //$response->getViewHelper('meta')->prepend('base', $base);
         //extract($response->getVariables(), EXTR_SKIP);
         // Set up Twig
-        $twig_loader = new FilesystemLoader();
-        $twig = new Environment($twig_loader); //TODO: Add caching
-        $twig->addGlobal('renderer', $this);
-        $twig->addExtension(new StringLoaderExtension());
-        $twig_loader->addPath($response->getViewPath());
-        $twig_loader->addPath($response->getLayoutPath()); //TODO: Add namespacing
+        $twig = $this->initTwig($response);
         $output['layout__file'] = $response->getLayoutFile() . '.twig';
         // Render the document
         echo $twig->render($response->getViewFile() . '.twig', $output);
     }
+
+    protected function initTwig(Response $response): Environment
+    {
+        $this->twig_loader = new FilesystemLoader();
+        $this->twig = new Environment($this->twig_loader); //TODO: Add caching
+        $this->twig->addGlobal('renderer', $this);
+        $this->twig->addExtension(new StringLoaderExtension());
+        $this->twig_loader->addPath($response->getViewPath());
+        $this->twig_loader->addPath($response->getLayoutPath()); //TODO: Add namespacing
+        $this->parseFilters($response);
+        return $this->twig;
+    }
+
+    protected function parseFilters(Response $response)
+    {
+        $filter_class_name = TwigFilter::class;
+        if (is_array($response->getValues()['twigFilters'])) {
+            foreach ($response->getValues()['twigFilters'] as $filter) {
+                if ($filter instanceof $filter_class_name) {
+                    $this->twig->addFilter($filter);
+                    var_dump('Added.');
+                }
+            }
+        }
+    }
+
 }
